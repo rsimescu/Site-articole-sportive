0b90d9d2f72aef6af2d19810f0ddad988d912aeb
rsimescu <148116341+rsimescu@users.noreply.github.com>
2024-12-17 22:47:44 +0200

    Final Site

--- aboutUs.js
+++ aboutUs.js
@@ -1,4 +1,5 @@
 const animatedTitlePart = document.querySelector(".animatedTitlePart");
+const hiddenElements = document.querySelectorAll(".hidden");
 
 const words = [
   "Athletes",
@@ -8,15 +9,28 @@ const words = [
   "Teams",
   "Performance",
   "Players",
-  "Passionate"
+  "Passionate",
 ];
 
 let currentIndex = 0;
 
 const textAnimation = () => {
   animatedTitlePart.textContent = words[currentIndex];
-  currentIndex = (currentIndex + 1) % words.length; 
+  currentIndex = (currentIndex + 1) % words.length;
 };
 
-textAnimation(); 
-setInterval(textAnimation, 5000); 
+textAnimation();
+setInterval(textAnimation, 5000);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el))
